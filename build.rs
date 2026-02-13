c6d94c5766213b57c9d3f35d0a3f8799d417a818
Galson Ngeywo <rutocalson56@gmail.com>
2026-02-13 09:53:48 +0300

    fix: add include paths to build.rs for github actions

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    slint_build::compile("ui/Main.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new()
+        // This tells the robot to look in the "ui" folder for any imports
+        .with_include_paths(vec![std::path::PathBuf::from("ui")]);
+
+    slint_build::compile_with_config("ui/Main.slint", config).expect("Slint build failed");
 }
